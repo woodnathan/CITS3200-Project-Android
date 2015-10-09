275ecbbf74e3a46edacc4f0e8057e5a6896cce8b
JoshuaWilkes <joshua.wilkes@hotmail.com>
2015-10-09 11:33:28 +0800

    Fixed some issues with calls for starting the edit screen

--- Breastfeeding_beta/app/src/main/java/com/yifeilyf/breastfeeding_beta/list.java
+++ Breastfeeding_beta/app/src/main/java/com/yifeilyf/breastfeeding_beta/list.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -29,6 +32,15 @@ public class list extends Activity {
     ListView lv;
     ArrayAdapter<String> adapter;
 
+    //Initialise backing data structure variables
+
+    private int feedCount = 0;
+    private ArrayList<Feed> feedBackingArray = new ArrayList<Feed>();
+
+    //UI objects
+    ListView feedList; //Scrollable list of feeds
+    ArrayAdapter<Feed> feedListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,27 +54,115 @@ public class list extends Activity {
 
         setContentView(R.layout.list);
 
+        //Initialise the backing data structure for the feeds list
+        feedList = (ListView) findViewById(R.id.feedList);
+        feedListAdapter = new ArrayAdapter<Feed>(this,
+                android.R.layout.simple_list_item_1,feedBackingArray);
+        feedList.setAdapter(feedListAdapter);
+
+        //TODO: List can be initialised with data retrieved from the server
+
+        //Setup the click listener for the feed list, to allow for editing
+        feedList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View v, int position, long id) {
+                Feed existingFeed = (Feed) adapter.getItemAtPosition(position);
+                Intent intent = new Intent(list.this, edit.class);
+
+
+                //TODO: Should this naming string be a global variable
+                intent.putExtra("com.yifeilyf.breastfeeding_beta.newFeed", existingFeed);
+
+                intent.putExtra("com.yifeilyf.breastfeeding_beta.editRequestCode", 1);
+                //RequestCode is the identifier sent to the edit activity, 0=newFeed 1=editFeed
+                //Open the edit activity with the existing feed for editing
+                startActivityForResult(intent, 1);
+            }
+        });
+
         //plus button
         Button btnPlus = (Button) findViewById(R.id.PlusButton);
 
         btnPlus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Feed newFeed= new Feed(feedCount++);
                 Intent intent = new Intent ();
                 intent.setClass(list.this, edit.class);
-                //startActivityForResult(intent, REQUEST_CODE);
-                startActivity(intent);
+                //
+
+                //The Feed object is a parcelable object type so that passing data can be kept simple
+
+                //TODO: Should this naming string be a global variable
+                intent.putExtra("com.yifeilyf.breastfeeding_beta.newFeed", newFeed);
+                intent.putExtra("com.yifeilyf.breastfeeding_beta.editRequestCode", 0);
+                //RequestCode is the identifier sent to the edit activity, 0=newFeed 1=editFeed
+
+                startActivityForResult(intent, 0);
             }
         });
     }
 
-    /*protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if(requestCode == REQUEST_CODE){
-            if(resultCode == edit.RESULT_CODE){
-                Bundle bundle = data.getExtras();
-                String
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //TODO: Add code to update the database with new feed data
+        //TODO: Make the list sort by date/time
+        //requestCode is the identifier sent to the edit activity, 0=newFeed 1=editFeed
+        if(resultCode != RESULT_CANCELED) {
+            if(requestCode == 0) {
+                Feed editedFeed = data.getParcelableExtra("com.yifeilyf.breastfeeding_beta.editedFeed");
+                //TODO Error handling?
+                addNewFeed(editedFeed);
+                feedListAdapter.notifyDataSetChanged();
+            } else if(requestCode == 1) {
+                Feed editedFeed = data.getParcelableExtra("com.yifeilyf.breastfeeding_beta.editedFeed");
+                //TODO Error handling?
+                updateFeed(editedFeed);
+                feedListAdapter.notifyDataSetChanged();
             }
         }
-    }*/
+    }
 
+    /**
+     * Private method to be used to update a feed in the feed list
+     * @param feedEdited A feed to replace an exiting feed with the same ID
+     * @return true on successful update, else false
+     */
+    private boolean updateFeed(Feed feedEdited) {
+        //Performs checks to ensure integrity of the feeds array
+        int id = feedEdited.getID();
+        for(int i = 0; i < feedBackingArray.size(); i++) {
+            if(feedBackingArray.get(i) != null) {
+                if(feedBackingArray.get(i).getID() == id) {
+                    feedBackingArray.set(i,feedEdited);
+                    //feed with same ID is found and updated successfully
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    /**
+     * Private method to be used to add a new feed to the list
+     * @param newFeed A feed object to be added to the list
+     * @return false if feed with the same ID is already in the list, else true
+     */
+    private boolean addNewFeed(Feed newFeed) {
+        //Performs checks to ensure integrity of the feeds array
+        int id = newFeed.getID();
+        for(int i = 0; i < feedBackingArray.size(); i++) {
+            if(feedBackingArray.get(i) != null) {
+                if(feedBackingArray.get(i).getID() == id) {
+                    //A feed with the same ID is already contained in the array
+                    return false;
+                }
+            }
+        }
+        feedBackingArray.add(newFeed);
+        return true;
+    }
 }

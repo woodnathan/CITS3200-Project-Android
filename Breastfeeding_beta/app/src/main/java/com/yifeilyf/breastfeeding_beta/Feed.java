da354264bf32a5a0b7a4087cac88bb699ba67d4f
JoshuaWilkes <joshua.wilkes@hotmail.com>
2015-10-09 09:21:04 +0800

    Added all data fields to the Feed object, updated required parcelable code.

--- Breastfeeding_beta/app/src/main/java/com/yifeilyf/breastfeeding_beta/Feed.java
+++ Breastfeeding_beta/app/src/main/java/com/yifeilyf/breastfeeding_beta/Feed.java
@@ -8,28 +8,127 @@ import android.os.Parcelable;
  * Data object to record feeds
  */
 public class Feed implements Parcelable {
-    //TODO: declare all the required data variables
-    private int data;
+
     private int ID;
+    private String startDate = "";
+    private String endDate = "";
+    private int startTime = -1;
+    private int endTime = -1;
+    private int type = -1; //(0,1,2) breastfeed expressed supplementary
+    private int subType = -1; // (0,1) left,right or expressed,supplementary
+    private double weightBefore = -1;
+    private double weightAfter = -1;
+    private String comment = "";
+
+    /**
+     *
+     *
+     */
     public Feed(int ID) {
         this.ID = ID;
     }
 
+
     /**
      *
-     * @return Integer - The ID of the Feed object
+     * @return
+     */
+    public String getStartDate() {return startDate;}
+    /**
+     *
+     * @return
+     */
+    public void putStartDate(String date) {startDate = date;}
+    /**
+     *
+     * @return
+     */
+    public String getEndDate() {return endDate;}
+    /**
+     *
+     * @return
+     */
+    public void putEndDate(String date) {endDate = date;}
+    /**
+     *
+     * @return
+     */
+    public int getStartTime() {return startTime;}
+    /**
+     *
+     * @return
+     */
+    public void putStartTime(int time) {startTime = time;}
+    /**
+     *
+     * @return
+     */
+    public int getEndTime() {return endTime;}
+    /**
+     *
+     * @return
+     */
+    public void putEndTime(int time) {endTime = time;}
+    /**
+     *
+     * @return
+     */
+    public int getType() {return type;}
+    /**
+     *
+     * @return
+     */
+    public void putType(int type) {this.type = type;}
+    /**
+     *
+     * @return
+     */
+    public int getSubType() {return subType;}
+    /**
+     *
+     * @return
+     */
+    public void putSubType(int type) {subType = type;}
+    /**
+     *
+     * @return
+     */
+    public double getWeightBefore() {return weightBefore;}
+    /**
+     *
+     * @return
+     */
+    public void putWeightBefore(double weight) {weightBefore = weight;}
+    /**
+     *
+     * @return
+     */
+    public double getWeightAfter() {return weightAfter;}
+    /**
+     *
+     * @return
+     */
+    public void put(double weight) {weightAfter = weight;}
+    /**
+     *
+     * @return
+     */
+    public String getComment() {return comment;}
+    /**
+     *
+     * @return
+     */
+    public void put(String comment) {this.comment = comment;}
+
+    /**
+     *
+     * @return
      */
     public int getID(){return ID;}
-    public void setData(int data) {
-        this.data = data;
 
-    }
-    public int getData(){
-        return data;
-    }
+
     public String toString() {
-        return "Feed " + getData();
-
+        return "Feed " + getID();
     }
 
 
@@ -40,13 +139,29 @@ public class Feed implements Parcelable {
 
     private Feed(Parcel in) {
         ID = in.readInt();
-        data = in.readInt();
+        startDate = in.readString();
+        endDate = in.readString();
+        startTime= in.readInt();
+        endTime= in.readInt();
+        type= in.readInt();
+        subType= in.readInt();
+        weightBefore= in.readDouble();
+        weightAfter= in.readDouble();
+        comment= in.readString();
 
     }
     @Override
     public void writeToParcel(Parcel out, int flags) {
         out.writeInt(ID);
-        out.writeInt(data);
+        out.writeString(startDate);
+        out.writeString(endDate);
+        out.writeInt(startTime);
+        out.writeInt(endTime);
+        out.writeInt(type);
+        out.writeInt(subType);
+        out.writeDouble(weightBefore);
+        out.writeDouble(weightAfter);
+        out.writeString(comment);
 
     }
 
